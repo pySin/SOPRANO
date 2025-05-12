2002218613bd460fe997f258eca31152bbcb11a8
pySin <sinan_km@yahoo.com>
2025-05-12 15:40:54 +0100

    Count synonymous and non-synonymous mutations.

--- SOPRANO_Python/in_imnpptd_mut.py
+++ SOPRANO_Python/in_imnpptd_mut.py
@@ -13,7 +13,7 @@ class OnMutations:
         # TO DO make sure the file is proper before opening
         # "with" closes the file after use so all data must be extracted before closing
         with open(self.mutations_file, "r", encoding="UTF8") as mf:
-            print(f"Mutations File: {mf}")
+            # print(f"Mutations File: {mf}")
 
             for line in mf.readlines():
                 on_mut_data.append(line.strip().split("	"))
@@ -36,11 +36,24 @@ class OnMutations:
         mut_trans_ids = [m_id[4] for m_id in self.on_mutations_data()]
         imm_trans_ids = [im[0] for im in self.on_immunopeptidome_data()]
 
-        print(f"Mutation IDs: {mut_trans_ids}")
+        # print(f"Mutation IDs: {mut_trans_ids}")
         # print(f"Immunopeptidome IDs: {imm_trans_ids}")
         match_trans_ids = [match_id for match_id in mut_trans_ids if match_id in imm_trans_ids]
-        print(f"Match Trans: {match_trans_ids}")
-        print(f"Matches Length: {len(match_trans_ids)}")
+        # print(f"Match Trans: {match_trans_ids}")
+        # print(f"Matches Length: {len(match_trans_ids)}")
+
+    def on_dnds(self):
+        on_m_data = self.on_mutations_data()
+        print(f"On DnDsData: {on_m_data}")
+        mutation_variants = [mut_v[6] for mut_v in on_m_data][1:]
+        print(f"Mutations Variants: {mutation_variants}")
+        print(f"Mutations Count: {len(mutation_variants)}")
+
+        synonymous_mutations = mutation_variants.count("synonymous_variant")
+        non_synonymous_mutations = mutation_variants.count("missense_variant")
+
+        print(f"synonymous_mutations: {synonymous_mutations}")
+        print(f"non_synonymous_mutations: {non_synonymous_mutations}")
 
 
 
--- SOPRANO_Python/main.py
+++ SOPRANO_Python/main.py
@@ -1,13 +1,13 @@
-# Soprano Pytho main caller
+# Soprano Python main caller
 import in_imnpptd_mut
 
 
 def caller():
     data_files = "data_files"
+    imnpptd_file = "MISSONI_ON_clonal_cohort_SOPRANO.anno"
     mutations_file = "IP_MISSONI_clonal_intersection_complement_modified.bed"
-    imnppdt_file = "MISSONI_ON_clonal_cohort_SOPRANO.anno"
 
-    mf_path = f"{data_files}/{imnppdt_file}"
+    mf_path = f"{data_files}/{imnpptd_file}"
     if_path = f"{data_files}/{mutations_file}"
     print(f"Mutations path: {mf_path}")
 
@@ -15,6 +15,7 @@ def caller():
     # on_mut.on_immunopeptidome_data()
     # on_mut.on_mutations_data()
     on_mut.mut_imnpptd_match()
+    on_mut.on_dnds()
 
 
 if __name__ == "__main__":
